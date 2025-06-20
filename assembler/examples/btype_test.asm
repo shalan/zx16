4b2d540894d3d2a22dd8b2f0bc2804032d41ef15
Mohamed Shalan <mshalan@aucegypt.edu>
2025-06-20 11:04:18 +0300

    Delete assembler/examples directory

--- assembler/examples/btype_test.asm
+++
@@ -1,8 +0,0 @@
-.org 0x0000
-.text
-start:
-	li t0, 5
-	bz t0, test
-
-test:
-	li t0, 0
--- assembler/examples/xori.asm
+++
@@ -1,8 +0,0 @@
-.text
-    .org    0
-main:
-    li      a0, 0x55           
-    xori    a0, 0x0F       
-    ecall   1                 
-
-    ecall   3               
